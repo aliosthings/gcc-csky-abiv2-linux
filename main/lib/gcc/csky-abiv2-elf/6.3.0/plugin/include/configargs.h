2534f8f78d76853ada4dce7e3fdbc87535e36a23
Li Cheng <lc122798@alibaba-inc.com>
2018-04-17 15:13:10 +0800

    add gcc-csky-abiv2 6.3.0 toolchain

---
+++ main/lib/gcc/csky-abiv2-elf/6.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/source//gcc/configure x86_64-linux --enable-languages=c,c++ --disable-threads --enable-sjlj-exceptions --disable-shared --enable-libssp --target=csky-abiv2-elf --prefix=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/install/ --with-headers=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/install//csky-abiv2-elf/include --with-lib=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/install//csky-abiv2-elf/lib/ --with-mpfr=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/lib-for-gcc-4.5-x86_64-linux/ --with-gmp=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/lib-for-gcc-4.5-x86_64-linux/ --with-mpc=/home/rtos_nfs/toolsbuild/slave-64/workspace/tools3_build_64_elf/abiv2_elf/lib-for-gcc-4.5-x86_64-linux/ --with-cskyabi=abiv2 --with-pkgversion='C-SKY Tools V3.4.0 Minilibc abiv2 B20180127'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
